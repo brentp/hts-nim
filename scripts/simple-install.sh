f30153d42108fd9a83e2a95a9129598a72f569f4
Brent Pedersen <bpederse@gmail.com>
2018-10-02 15:05:36 -0600

    simplify simple install

--- scripts/simple-install.sh
+++ scripts/simple-install.sh
@@ -11,25 +11,10 @@ set -x
 
 git clone -b $BRANCH --depth 1 git://github.com/nim-lang/nim nim-$BRANCH/
 cd nim-$BRANCH
-git clone --depth 1 git://github.com/nim-lang/csources csources/
-cd csources
-sh build.sh
-cd ..
-rm -rf csources
-bin/nim c koch
-./koch boot -d:release
-
-export PATH=$PATH:$base/nim-$BRANCH/bin/:$PATH:$base/nimble/src
-echo $PATH
-
-cd $base
-git clone --depth 1 https://github.com/nim-lang/nimble.git
-cd nimble
-nim c src/nimble
-src/nimble install -y
+sh build_all.sh
 
 cd $base
 set -x
 nimble refresh
 
-echo "set PATH=$base/nim-$BRANCH/bin/:$base/nimble/src:"'$PATH' to have this in your path.
+echo "set PATH=$base/nim-$BRANCH/bin/":'$PATH' to have this in your path.
