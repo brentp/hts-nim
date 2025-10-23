63ae34cf0f3d9c978b6bfd4d847bda462dc55d01
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2025-10-23 07:25:48 +0100

    in Nim 2.2.4 temporaries are no longer allowed; result.left.cdouble creates a temporary C double value

--- src/hts/stats.nim
+++ src/hts/stats.nim
@@ -14,7 +14,12 @@ proc `$`*(f:fisher_result): string =
 # kt_fisher_exact(int n11, int n12, int n21, int n22, double *_left
 proc fishers_exact_test*[T:int|int64|int32](n11:T, n12:T, n21:T, n22:T): fisher_result {.inline.} =
   result = fisher_result()
-  discard kt_fisher_exact(n11.cint, n12.cint, n21.cint, n22.cint, result.left.cdouble.addr, result.right.cdouble.addr, result.two.cdouble.addr)
+  # Cast the address to ptr cdouble if needed
+  discard kt_fisher_exact(n11.cint, n12.cint, n21.cint, n22.cint, 
+                          cast[ptr cdouble](addr result.left),
+                          cast[ptr cdouble](addr result.right),
+                          cast[ptr cdouble](addr result.two))
+
 
 proc binom_test*[T:int|int64|int32|uint32|uint64|uint16](successes:T, trials:T): float64 {.inline.} =
   if trials == 0: return 1
